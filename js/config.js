cbade6bad37a6df6fa6069bd0a6b9ddbdb7f944d
zizih <zizihjk@gmail.com>
2013-11-07 09:46:58 +0800

    modify i

--- js/config.js
+++ js/config.js
@@ -12,19 +12,19 @@ app.config = {
         imgUrl: './images/hacker01.JPG',
         imgAlt: '2',
         href: 'https://plus.google.com/photos/114920001331857622210/albums/5830155029390732513?authkey=CNPT-Y-V3fqJ4gE',
-        caption: '人生中见到第一批程序员到此时着实除了一个android加减乘除计算器和js计时器便没有任何成果了，也敢在大牛云集的bata提点子,点子是《手机防摔保护程序》。'
+        caption: '人生中见到第一批程序员到此时着实除了一个android加减乘除计算器和js计时器便没有任何成果了，也敢在大牛云集的bata提点子,点子是《手机防摔保护程序》。——第四届广州高校编程马拉松'
       },
       {
         imgUrl: './images/hacker02.JPG',
         imgAlt: '2',
         href: 'https://plus.google.com/photos/114920001331857622210/albums/5830155029390732513?authkey=CNPT-Y-V3fqJ4gE',
-        caption: '这个位置是现场组的一位队友，和我一样的编程基础，并且在在马拉松进行了6个小时之后和我道别，我孤军挣扎了一会也只好去吃饭了。'
+        caption: '这个位置是现场组的一位队友，和我一样的编程基础，在马拉松进行了6个小时之后和我道别，我孤军挣扎了一会也只好去吃饭了。'
       },
       {
         imgUrl: './images/hacker03.JPG',
         imgAlt: '3',
         href: 'https://plus.google.com/photos/114920001331857622210/albums/5830155029390732513?authkey=CNPT-Y-V3fqJ4gE',
-        caption: '实现功能的想法是利用手机自带的加速度传感器，计算和判断非正常移动则采取相应措施（最后是没有找到可行的方法），和GDG三位导师做自行车助手的想法扯上了一点关系，然后anson收留我一起写bikeplus。'
+        caption: '实现自我保护功能的想法是利用手机自带的加速度传感器，计算和判断非正常移动则采取措施（最后是没有找到可行的措施），和GDG三位导师做自行车助手的想法扯上了一点关系，然后anson收留我一起写bikeplus。'
       },
       {
         imgUrl: './images/hacker04.JPG',
@@ -39,25 +39,25 @@ app.config = {
         imgUrl: './images/codejam00.JPG',
         imgAlt: '1',
         href: 'http://segmentfault.com/e/codejam-guangzhou-2013-p3',
-        caption: '2013中国移动开发者大赛，两个人的组合（鹏飞：硬件出生的软件工程师 大宝：硬件工程师）需要一个安卓开发，没有任何准备的我有幸加入。'
+        caption: '两个人的组合（鹏飞：硬件出生的软件工程师 大宝：硬件工程师）需要一个安卓开发，没有任何准备的我有幸加入。<br>——2013中国移动开发者大赛'
       },
       {
         imgUrl: './images/codejam01.JPG',
         imgAlt: '1',
         href: 'http://segmentfault.com/e/codejam-guangzhou-2013-p3',
-        caption: '编程进行了24个小时之后，我们《唤醒灯》成果展示时间，鹏飞在向参赛者解说，我在幕后操作手机客户端，屏幕上看到的黑白时间定时界面就是我风雨不动摇的黑白风格了。'
+        caption: '编程进行了24个小时之后，我们《唤醒灯》成果展示时间，鹏飞向参赛者解说，我在幕后操作手机客户端，屏幕上看到的黑白时间定时界面就是我风雨不动摇的黑白风格了。'
       },
       {
         imgUrl: './images/codejam02.JPG',
         imgAlt: '2',
         href: 'http://segmentfault.com/e/codejam-guangzhou-2013-p3',
-        caption: '演示进行得差不多了，大家都在等手机遥控关灯结果出意外了，android和单片机的连接超时已经断开，求原谅。'
+        caption: '灯已经慢慢亮到可以唤醒睡眠的人，大家都在等遥控关灯，结果android和单片机的连接超时已经断开，求原谅。'
       },
       {
         imgUrl: './images/codejam03.JPG',
         imgAlt: '3',
         href: 'http://segmentfault.com/e/codejam-guangzhou-2013-p3',
-        caption: '颁奖的时候，很对不起鹏飞，我写android时没有考虑周全令演示出状况得了第四名,后来主办方计算了作品数量解释说第四名开始是优秀奖，好吧，就是第四名。'
+        caption: '颁奖的时候，很对不起鹏飞，演示出状况得了第四名，后来主办方计算了作品数量解释说第四名开始是优秀奖，好吧，就是第四名。'
       }
     ],
     intern: [
@@ -65,27 +65,32 @@ app.config = {
         isFirst: true,
         imgUrl: './images/intern01.jpg',
         imgAlt: '1',
-        caption: '“暑假三个月实习之后就可以拿到实习学分了”是我最真实和最傻的全部想法,后来资深人士告诉我“留在公司”才应该是实习最大的“企图”。'
+        caption: '“三个月实习之后就可以拿到学分了”，是当时最真最傻的全部想法。'
       },
       {
         imgUrl: './images/intern02.jpg',
         imgAlt: '2',
-        caption: '20120410 - 20130709, 一年零三个月陪伴我所有的编程之路的操作系统，但求助了linux大牛也没能成功编译tplink55××驱动，解决办法是换网卡。'
+        caption: '一年零三个月陪伴我所有的编程之路的操作系统，但求助了linux大牛也没能成功编译tplink55××驱动，解决办法是换网卡。'
       },
       {
         imgUrl: './images/intern03.jpg',
         imgAlt: '3',
-        caption: '身边的正式员工，他们很忙我很少和他们交流，这块地方总是停电和服务器down机，以至于最初的开发过程满满的都是技术和硬件的挑战。'
+        caption: '他们很忙我很少和他们交流，这块地方总是停电和服务器down机，以至于最初的开发过程满满的都是技术和硬件的挑战。'
       },
       {
         imgUrl: './images/intern04.jpg',
         imgAlt: '4',
-        caption: '参加了个性壁纸、九周年“陪伴成长 我的一件事”、办新家新闻推送、印度拉力赛暨新年活动，前后两个有参与java后台，中间两个完全前端，最后一个还没提测，对不起没用心，我不是反应迟钝，相信我！'
+        caption: '参加了四个线上活动的应用研发，参与Java后台又参与Javascript前端，加上没用心，我不是反应迟钝，相信我！'
       },
       {
         imgUrl: './images/intern05.jpg',
         imgAlt: '5',
-        caption: '不知道当初没有强烈留在公司的愿望会成真。三个月没有把握时间好好投入到项目中，知道自己要离职到离开公司只有几个小时，脑子究竟放空了多久呢！我还能回去吗？'
+        caption: '当初没有强烈留在公司的愿望成真了。<br>“推广产品和推倒妹子”一点也不好笑。'
+      },
+      {
+        imgUrl: './images/intern06.jpg',
+        imgAlt: '6',
+        caption: '为当初的轻浮付出的代价是——回不去的位置。<br>我准备好了，给我机会吗？'
       }
     ],
     hope: [
@@ -99,27 +104,47 @@ app.config = {
       {
         imgUrl: './images/hope02.JPG',
         imgAlt: '1',
-        caption: '背影是迷人的，头发是修理过的。'
+        caption: '背影迷人，头发修理过。'
       },
       {
         imgUrl: './images/hope03.jpg',
         imgAlt: '1',
-        caption: '台上最闪耀，淡定不猥琐的。'
+        caption: '台上最闪耀，淡定不猥琐。'
       },
       {
         imgUrl: './images/hope04.JPG',
         imgAlt: '1',
-        caption: '头发梳了，营养良了，身材和智商上是女汉子。'
+        caption: '头发梳了，营养良，才智双全。'
       },
       {
         imgUrl: './images/hope05.JPG',
         imgAlt: '1',
-        caption: '摄影师注意到每一个，包括我。'
+        caption: '有摄像头注意到我。'
       },
       {
         imgUrl: './images/hope06.jpg',
         imgAlt: '1',
-        caption: 'Maybe,big breasts needed.'
+        caption: '有数不清的朋友，3个，4个。。。。'
+      },
+      {
+        imgUrl: './images/hope07.jpg',
+        imgAlt: '1',
+        caption: '美丽回眸和长发飘飘。。'
+      },
+      {
+        imgUrl: './images/hope08.jpg',
+        imgAlt: '1',
+        caption: '活在爱丽丝梦游仙境。'
+      },
+      {
+        imgUrl: './images/hope09.jpg',
+        imgAlt: '1',
+        caption: '技术牛人，gt 2个屏幕'
+      },
+      {
+        imgUrl: './images/hope10.jpg',
+        imgAlt: '1',
+        caption: 'Maybe, big breasts too.'
       }
     ]
   },
@@ -139,7 +164,7 @@ app.config = {
     },
     {
       tagId: 3,
-      tagName: '希望总是空的'
+      tagName: '希望与现实'
     }
   ]
   
