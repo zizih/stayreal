2d648043d014fb82f07ecebee8391b86a57e989e
zizihjk <zizihjk@gmail.com>
2013-11-09 21:10:40 +0800

    后台添加对fonts请求的支持

---
+++ iserver/src/webapp/controller/BaseController.java
@@ -0,0 +1,145 @@
+package webapp.controller;
+
+import com.google.gson.Gson;
+import weblib.model.Header;
+import weblib.util.FileUtil;
+
+import java.io.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rain
+ * Date: 11/6/13
+ * Time: 5:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseController {
+
+    private Header header;
+    private PrintWriter respon;
+    private OutputStream responOS;
+
+    public void render(String templateName) {
+        respon.println("HTTP/1.1 200 OK");
+        renderUtil("text/html", template(templateName));
+    }
+
+    public void renderJSON(Object object) {
+        respon.println("HTTP/1.1 202 OK");
+        renderUtil("text/json", new Gson().toJson(object));
+    }
+
+    public void render404() {
+        respon.println("HTTP/1.1 404 Not Found");
+        renderUtil("text/html", "<404> iServer Sorry!");
+    }
+
+    public void renderOs2Js(InputStream is) {
+        respon.println("HTTP/1.1 200 OK");
+        respon.println("Content-Type:text/javascript;charset=UTF8");
+        respon.println();
+        renderOS(is);
+        respon.close();
+    }
+
+    public void renderOs2Css(InputStream is) {
+        respon.println("HTTP/1.1 200 OK");
+        respon.println("Content-Type:text/css;charset=UTF8");
+        respon.println();
+        renderOS(is);
+        respon.close();
+    }
+
+    public void renderOs2Image(InputStream is) {
+        respon.println("HTTP/1.1 200 OK");
+        respon.println("Content-Type:image/jpeg;charset=UTF8");
+        respon.println();
+        renderOS(is);
+        respon.close();
+    }
+
+    public void renderOs2Font(InputStream is) {
+        respon.println("HTTP/1.1 200 OK");
+        respon.println("Content-Type:font/opentype;charset=UTF8");
+        respon.println();
+        renderOS(is);
+        respon.close();
+    }
+
+    private void renderOS(InputStream is) {
+        byte[] buffer = new byte[2048];
+        int len;
+        try {
+            while ((len = is.read(buffer)) != -1) {
+                responOS.write(buffer, 0, len);
+            }
+            responOS.flush();
+            is.close();
+            responOS.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 按MIME类型返回文本
+     *
+     * @param contentType
+     * @param str
+     */
+    private void renderUtil(String contentType, String str) {
+        respon.println("Content-Type:" + contentType + ";charset=UTF8");
+        respon.println();
+        respon.println(str);
+        respon.flush();
+        respon.close();
+    }
+
+    /**
+     * 读取一个文件的内容并以二进制文件形式返回
+     */
+    public void renderFile(String fileName) {
+
+        try {
+            PrintStream out = new PrintStream(responOS, true);
+            File fileToSend = new File(fileName);
+            System.out.println("exist: " + fileToSend.exists());
+            if (fileToSend.exists() && !fileToSend.isDirectory()) {
+                out.println("HTTP/1.0 200 OK");//返回应答消息,并结束应答
+                out.println("Content-Type:application/binary");
+                out.println("Content-Length:" + fileToSend.length());// 返回内容字节数
+                out.println();// 根据 HTTP 协议, 空行将结束头信息
+
+                FileInputStream fis = new FileInputStream(fileToSend);
+                byte data[] = new byte[fis.available()];
+                fis.read(data);
+                out.write(data);
+                System.out.println(data);
+                out.close();
+                fis.close();
+            }
+        } catch (Exception e) {
+            System.out.println("传送文件时出错:" + e.getLocalizedMessage());
+        }
+    }
+
+    private String template(String templateName) {
+        return FileUtil.views(templateName).toString();
+    }
+
+    /**
+     * 共socket处理线程回调设置controller参数
+     */
+    public void setResponOS(OutputStream responOS) {
+        this.responOS = responOS;
+        respon = new PrintWriter(responOS, true);
+    }
+
+    /**
+     * 给Socket Server处理http请求时设置Header（未完成）
+     */
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+}
---
+++ iserver/src/weblib/net/SocketThread.java
@@ -0,0 +1,171 @@
+package weblib.net;
+
+import webapp.controller.Application;
+import webapp.controller.BaseController;
+import weblib.model.Header;
+import weblib.util.FileUtil;
+import weblib.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.Socket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rain
+ * Date: 11/6/13
+ * Time: 1:57 PM
+ * Desc: 每接受到一个socket请求就创建一个线程，并调用controller相应的路由控制。
+ */
+public class SocketThread extends Thread {
+
+    private BaseController controller;
+    private Socket client;
+
+    public SocketThread(Socket client) {
+        this.client = client;
+        this.controller = new Application();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            if (client == null) return;
+            Header header = new Header(client.getInputStream());
+            //暂时不处理/favicon.ico请求
+            if (header.getRequestURI() == null || header.getRequestURI().equals("/favicon.ico")) {
+                closeSocket(client);
+                return;
+            }
+            Log.i("request: " + header.getRequestURI());
+            String uri = header.getRequestURI();
+
+            //处理public资源请求,不需要暴露给controller处理
+            if (uri.startsWith("/public/css")) {
+                renderCSS(FileUtil.fileIS(uri));
+            }
+            if (uri.startsWith("/public/js")) {
+                renderJS(FileUtil.fileIS(uri));
+            }
+            if (uri.startsWith("/public/images")) {
+                renderImage(FileUtil.fileIS(uri));
+            }
+            if (uri.startsWith("/public/fonts")) {
+                renderFont(FileUtil.fileIS(uri));
+            }
+
+
+            //处理controller控制的请求
+            Class controllerClass = controller.getClass();
+            Method[] methods = controllerClass.getDeclaredMethods();
+            for (Method method : methods) {
+                if (uri == null) {
+                    closeSocket(client);
+                    return;
+                }
+                try {
+                    //匹配index路径
+                    if (uri.equals("/") && method.getName().equals("index")) {
+                        controller.setResponOS(client.getOutputStream());
+                        method.invoke(controller);
+                        return;
+                    }
+                    //匹配含有get参数的路径
+                    int qIndex = uri.indexOf("?");
+                    String path = uri.substring(1, qIndex == -1 ? uri.length() : qIndex);
+                    if (path.equals(method.getName())) {
+
+                        //获取controller参数个数
+                        int paramsLength = method.getParameterTypes().length;
+                        String[] paramsValues = new String[paramsLength];
+
+                        String[] params = null;
+                        if (qIndex != -1) {
+                            String paramstr = uri.substring(qIndex + 1, uri.length());
+                            params = paramstr.split("&");
+                        }
+
+                        //长度以controller请求中的参数长度为准
+                        for (int i = 0; i < paramsLength; i++) {
+                            if (params == null || params.length < i + 1) {
+                                paramsValues[i] = null;
+                            } else {
+                                paramsValues[i] = params[i].substring(params[i].indexOf("=") + 1,
+                                        params[i].length());
+                            }
+                        }
+                        controller.setResponOS(client.getOutputStream());
+                        method.invoke(controller, paramsValues);
+                        return;
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        closeSocket(client);
+    }
+
+    public void closeSocket(Socket socket) {
+        try {
+            if (client != null && !client.isClosed() && controller != null) {
+                controller.setResponOS(client.getOutputStream());
+                controller.render404();
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void renderJS(InputStream is) {
+        try {
+            controller.setResponOS(client.getOutputStream());
+            controller.renderOs2Js(is);
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        controller.render404();
+    }
+
+    private void renderCSS(InputStream is) {
+        try {
+            controller.setResponOS(client.getOutputStream());
+            controller.renderOs2Css(is);
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        controller.render404();
+    }
+
+    private void renderImage(InputStream is) {
+        try {
+            controller.setResponOS(client.getOutputStream());
+            controller.renderOs2Image(is);
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        controller.render404();
+    }
+
+    private void renderFont(InputStream is) {
+        try {
+            controller.setResponOS(client.getOutputStream());
+            controller.renderOs2Font(is);
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        controller.render404();
+    }
+}
