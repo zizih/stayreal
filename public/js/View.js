8f5b6679556fe91da157cb6688deda22e72bfe7d
zizihjk <zizihjk@gmail.com>
2013-11-13 16:35:22 +0800

    add album前端

--- public/js/View.js
+++ public/js/View.js
@@ -1,83 +1,88 @@
 app.View = function() {
-  
-  
 
   var IndexView = Backbone.View.extend({
 
-    el: $('#page-index'),
-    events: {
-      'keypress #comment': 'edit',
-      'keyup input': 'unedit',
-      'click #to-demo': 'todemo' 
+    el : $('#page-index'),
+    events : {
+      'keypress #comment' : 'edit',
+      'keyup input' : 'unedit',
+      'click #to-demo' : 'todemo'
     },
-    initialize: function() {
-      if(!app.iAjax) {
-        app.iAjax = new iAjax();
-      }
-      var that = this;
-      app.iAjax.get({
-        url: 'fetchComments',
-        success: function (data) {
-          for(i in data) {
-            that.render(new app.constr.models.CommentModel({
-              content: data[i].content,
-              id: data[i].id 
-            }).setTime(data[i].time));            
-          }
-        },
-        err: function(err) {
-          console.log(err);
+    initialize : function() {
+      try {
+        if (!app.iAjax) {
+          app.iAjax = new iAjax();
         }
-      });
+        var that = this;
+        app.iAjax.get({
+          url : 'fetchComments',
+          success : function(data) {
+            for (i in data) {
+              that.render(new app.constr.models.CommentModel({
+                content : data[i].content,
+                id : data[i].id
+              }).setTime(data[i].time));
+            }
+          },
+          err : function(err) {
+            console.log(err);
+          }
+        });
+      } catch (ex) {
+        var alert = new AlertView();
+        alert.show({
+          top : 500 + 'px',
+          left : 500 + 'px',
+          timeout : 3000,
+          firstContent : '不要ajax请求报错，先运行iserver吖  ~ ^_^ .... ',
+        });
+      }
     },
-    template: _.template($('#comment-template').html()),
-    render: function() {
+    template : _.template($('#comment-template').html()),
+    render : function() {
       return this;
     },
-    render: function(model) {
+    render : function(model) {
       $('#comments').append(this.template(model.toJSON()));
     },
-    add: function(str) {
+    add : function(str) {
       this.model.commentModel.updateTime().set('content', str);
       $('#comments').append(this.template(this.model.commentModel.toJSON()));
       this.save();
     },
-    save: function () {
-      console.log(this.model.commentModel.toJSON());
-      var model = this.model.commentModel,
-        url = 'insertComment?content='
-          + model.get('content');
-          console.log(url);
+    save : function() {
+      var model = this.model.commentModel, url = 'insertComment?content=' + model.get('content');
+      console.log(url);
       app.iAjax.get({
-        url: url,
+        url : url,
         //iserver post 请求还没有实现获取请求体
         // data: this.model.commentModel.toJSON(),
-        success: function (data) {
+        success : function(data) {
           console.log(data);
         },
-        err: function (err) {
+        err : function(err) {
           console.log();
         }
       });
     },
-    edit: function (e) {
-      if(e.keyCode == 13) {
+    edit : function(e) {
+      if (e.keyCode == 13) {
         var $target = $(e.target);
         this.add($target.val());
         $target.val('');
       }
     },
-    todemo: function(e) {
+    todemo : function(e) {
       var alert = new AlertView();
       alert.show({
-        top: (e.target.offsetTop + 200) + 'px',
-        left: (e.target.offsetLeft + 100) + 'px',
-        timeout: 1000,
-        firstContent: 'unfinished～',
+        top : (e.target.offsetTop + 200) + 'px',
+        left : (e.target.offsetLeft + 100) + 'px',
+        timeout : 1000,
+        firstContent : 'Unfinished Page ～ O_O ~',
       });
-      location.href="#server";
+      location.href = "#server";
     },
-    show: function () {
+    show : function() {
       this.$el.show();
       //modify nav item
       var item0 = $('#header-nav-item0');
@@ -85,153 +90,197 @@ app.View = function() {
       item0.attr('href', '#i');
       return this;
     },
-    hide: function () {
+    hide : function() {
       this.$el.hide();
     }
   });
-  
+
   var IView = Backbone.View.extend({
-    
-    el: $('#page-i'),
-    initialize: function () {
+
+    el : $('#page-i'),
+    initialize : function() {
       var item0 = $('#header-nav-item0');
       item0.html('home');
       item0.attr('href', '#index');
       //
-      this.workModel = this.model.works[2];
+      this.defaultAblum = 2;
+      this.jsWorks = this.model.works;
+      this.jsAblums = this.model.albums;
+      this.workModel = this.model.works[this.defaultAblum];
       this.workView = new WorksView({
-          model: this.workModel 
-        });
+        model : this.workModel
+      });
     },
-    events: {
-      'click .album a': 'album'
+    events : {
+      'click .album a' : 'album',
+      'click #to-iserver-album' : 'toiserver'
     },
-    template: _.template($('#works-album-template').html()),
-    show: function () {
+    template : _.template($('#works-album-template').html()),
+    show : function() {
       $('.album').html(this.template(this.model.albums.toJSON()));
       this.$el.show();
       return this;
     },
-    render: function () {
+    render : function() {
+      this.show();
       return this;
     },
-    album: function (e) {
+    album : function(e) {
       $target = $(e.target);
-      var index =parseInt($target.attr('data-tag-index'));
-      this.workModel = this.model.works[index];
+      var index = parseInt($target.attr('data-tag-index'));
+      this.workModel = this.model.works[index-1];
       this.workView.resetModel(this.workModel).render();
       //tag
       $('.album').find('a').each(function(target, dom) {
         $(dom).removeClass('select');
       });
       $target.addClass('select');
+    },
+    toiserver : function(e) {
+      var $target = $(e.target);
+      console.log($target.attr('data-src'));
+      if($target.attr('data-src') === 'server') {
+        this.model.works = this.jsWorks;
+        this.model.albums = this.jsAblums;
+        this.workView.resetModel(this.model.works[this.defaultAblum]).render();
+        this.render(); //关于album名字的渲染
+        $target.attr('data-src','jsconf').html('切换到后台');
+      } else if($target.attr('data-src') === 'jsconf'){
+        try {
+          if (!app.iAjax) {
+            app.iAjax = new iAjax();
+          }
+          var that = this;
+          app.iAjax.get({
+            url : 'fetchAlbums',
+            success : function(data) {
+              that.model.albums = new app.constr.models.AblumModel({
+                album : data.albums
+              });
+              var carouselArr = [];
+              for(i in data.carousels) {
+                carouselArr[i] = new app.constr.models.WorksModel({
+                  work: data.carousels[i]
+                }); 
+              }
+              that.model.works = carouselArr;
+              that.workView.resetModel(that.model.works[0]).render();
+              that.render();
+              $target.attr('data-src','server').html('切换到前端');
+            },
+            err : function(err) {
+              console.log(err);
+            }
+          });
+        } catch (ex) {
+          //deal with ex  
+        }
+      }
     }
   });
-  
+
   var WorksView = Backbone.View.extend({
-    el: $('#myworks'),
-    initialize: function () {
+    el : $('#myworks'),
+    initialize : function() {
       $('.carousel').carousel({
-        interval: 10000
+        interval : 10000
       });
       // this.listenTo(this.model, 'change', this.render); 没有产生作用
       this.render();
     },
-    events: {
-      'click .carousel-inner img': 'official',  
+    events : {
+      'click .carousel-inner img' : 'official',
     },
-    template: {
-      works: _.template($('#works-item-template').html()),
-      indicators: _.template($('#works-indicator-template').html())
+    template : {
+      works : _.template($('#works-item-template').html()),
+      indicators : _.template($('#works-indicator-template').html())
     },
-    render: function () {
+    render : function() {
       $('.carousel-inner').html(this.template.works(this.model.toJSON()));
       $('.carousel-indicators').html(this.template.indicators({
-        len: this.model.getSize()
+        len : this.model.getSize()
       }));
     },
-    official: function (e){
+    official : function(e) {
       var href = $(e.target).data().href;
-      if(!!href) {
+      if (!!href) {
         window.open(href);
       }
     },
-    resetModel: function (obj) {
+    resetModel : function(obj) {
       this.model = obj;
       return this;
-    } 
+    }
   });
-  
+
   var ServerView = Backbone.View.extend({
-    el: $('#page-server'),
-    initialize: function() {
+    el : $('#page-server'),
+    initialize : function() {
       var item0 = $('#header-nav-item0');
       item0.html('home');
       item0.attr('href', '#index');
     },
-    events: {
-      'mouseover .server-item .server-item-title': 'showdesc',
-      'mouseout .server-item .server-item-title': 'hidedesc',
-      'click .server-item .server-item-title': 'showop',
-      'dblclick .server-item .server-item-title': 'hideop',
-      'click #insert': 'insert'
+    events : {
+      'mouseover .server-item .server-item-title' : 'showdesc',
+      'mouseout .server-item .server-item-title' : 'hidedesc',
+      'click .server-item .server-item-title' : 'showop',
+      'dblclick .server-item .server-item-title' : 'hideop',
+      'click #insert' : 'insert'
     },
-    template: {
+    template : {
       //_.template($('#').html())
     },
-    insert: function () {
+    insert : function() {
       $.ajax({
-        type: 'get',
-        url: 'insert?name=rain&jack=arron',
-        success: function (data) {
+        type : 'get',
+        url : 'insert?name=rain&jack=arron',
+        success : function(data) {
           console.log(data);
-        } 
+        }
       });
     },
-    showdesc: function (e) {
+    showdesc : function(e) {
       $(e.target).next().show();
     },
-    hidedesc: function (e) {
+    hidedesc : function(e) {
       $(e.target).next().hide();
     },
-    showop: function (e) {
+    showop : function(e) {
       this.showdesc(e);
       this.hideAllOp(e);
       $(e.target).next().next().toggle();
       e.preventDefault();
     },
-    hideop: function (e) {
+    hideop : function(e) {
       this.hidedesc(e);
       $(e.target).next().next().hide();
       e.preventDefault();
     },
-    hideAllOp: function (e) {
+    hideAllOp : function(e) {
       $(e.target).parents().find('.server-item-op').hide();
     },
-    render: function(){
-      
+    render : function() {
+
     },
-    show: function() {
+    show : function() {
       this.$el.show();
       return this;
     }
   });
 
   var AlertView = Backbone.View.extend({
-    el: $('body'),
+    el : $('body'),
     initialize : function() {
-      this.top = (screen.height - 50) + 'px',
-      this.left = (screen.width - 50) + 'px',
-      this.timeout = 1000;
+      this.top = (screen.height - 50) + 'px', this.left = (screen.width - 50) + 'px', this.timeout = 1000;
       this.firstContent = '有错啦。。。';
     },
-    template: _.template($('#alert-template').html()),
-    show: function(options) {
+    template : _.template($('#alert-template').html()),
+    show : function(options) {
       var that = this;
       this.alertView = $(this.template({
-        top: options.top || this.top,
-        left: options.left || this.left,
-        firstContent: options.firstContent || this.firstContent
+        top : options.top || this.top,
+        left : options.left || this.left,
+        firstContent : options.firstContent || this.firstContent
       }));
       this.$el.append(this.alertView);
       this.alertView.show();
@@ -244,9 +293,9 @@ app.View = function() {
   });
 
   return {
-    IndexView: IndexView,
-    IView: IView,
-    ServerView: ServerView
+    IndexView : IndexView,
+    IView : IView,
+    ServerView : ServerView
   }
 
 }
--- public/js/config.js
+++ public/js/config.js
@@ -8,178 +8,184 @@ app.config = {
   works: {
     gzhacker: [
       {
-        isFirst: true,
-        imgUrl: './public/images/hacker01.JPG',
-        imgAlt: '2',
+        isfirst: true,
+        imgurl: './public/images/hacker01.JPG',
+        imgalt: '2',
         href: 'https://plus.google.com/photos/114920001331857622210/albums/5830155029390732513?authkey=CNPT-Y-V3fqJ4gE',
-        caption: '人生中见到第一批程序员到此时着实除了一个android加减乘除计算器和js计时器便没有任何成果了，也敢在大牛云集的bata提点子,点子是《手机防摔保护程序》。——第四届广州高校编程马拉松'
+        caption: '人生中见到第一批程序员（2012年4月10日加入HCI）到此时除了android加减乘除计算器和js计时器demo便什么都不会了，也在大牛云集的bata提点子,点子是《手机防摔保护程序》。——第四届广州高校编程马拉松 2012年8月'
       },
       {
-        imgUrl: './public/images/hacker02.JPG',
-        imgAlt: '2',
+        imgurl: './public/images/hacker02.JPG',
+        imgalt: '2',
         href: 'https://plus.google.com/photos/114920001331857622210/albums/5830155029390732513?authkey=CNPT-Y-V3fqJ4gE',
-        caption: '这个位置是现场组的唯一一位队友，和我一样的编程基础，在马拉松进行了6个小时之后和我道别，我孤军挣扎了一会也只好去吃饭了。'
+        caption: '右边是现场组的唯一一位队友，和我一样的编程基础，在马拉松进行了6个小时之后和我道别，我孤军挣扎了一会也只好去晚饭了。'
       },
       {
-        imgUrl: './public/images/hacker03.JPG',
-        imgAlt: '3',
+        imgurl: './public/images/hacker03.JPG',
+        imgalt: '3',
         href: 'https://plus.google.com/photos/114920001331857622210/albums/5830155029390732513?authkey=CNPT-Y-V3fqJ4gE',
-        caption: '实现自我保护功能的想法是利用手机自带的加速度传感器，计算和判断非正常移动则采取措施（最后是没有找到可行的措施），和GDG三位导师做自行车助手的想法扯上了一点关系，然后anson收留我一起写bikeplus。'
+        caption: '实现自我保护功能是利用手机自带的加速度传感器，通过三维加速度计算和判断非正常移动则采取措施，同伴离开计划失败后，和GDG三位导师实现自行车助手的技术一致，然后anson收留我一起写bikeplus。'
       },
       {
-        imgUrl: './public/images/hacker04.JPG',
-        imgAlt: '4',
+        imgurl: './public/images/hacker04.JPG',
+        imgalt: '4',
         href: 'https://plus.google.com/photos/114920001331857622210/albums/5830155029390732513?authkey=CNPT-Y-V3fqJ4gE',
-        caption: '没有做出什么成果，在anson的指导下完成了条形图的绘制，然后凌晨在2楼露天院子的沙发上睡了5个小时，但是第一次24编程的体会是：我将来可以成为程序员。'
+        caption: '没有做出什么成果，在anson的指导下完成了条形图的绘制，然后凌晨在2楼露天院子的沙发上睡了5个小时，第一次24编程的体会是：我将来要成为程序员。'
       }
     ],
     codejam: [
       {
-        isFirst: true,
-        imgUrl: './public/images/codejam00.JPG',
-        imgAlt: '1',
+        isfirst: true,
+        imgurl: './public/images/codejam00.JPG',
+        imgalt: '1',
         href: 'http://segmentfault.com/e/codejam-guangzhou-2013-p3',
-        caption: '两个人的组合（鹏飞：硬件出生的软件工程师 大宝：硬件工程师）需要一个安卓开发，没有任何准备的我有幸加入。<br>——2013中国移动开发者大赛'
+        caption: '两人组合（鹏飞：硬件出生的软件工程师 大宝：硬件工程师）需要一个安卓开发，没有任何同伴也没有任何准备的我有幸被邀请加入。<br>——2013中国移动开发者大赛'
       },
       {
-        imgUrl: './public/images/codejam01.JPG',
-        imgAlt: '1',
+        imgurl: './public/images/codejam01.JPG',
+        imgalt: '1',
         href: 'http://segmentfault.com/e/codejam-guangzhou-2013-p3',
-        caption: '编程进行了24个小时之后，我们《唤醒灯》成果展示时间，鹏飞向参赛者解说，我在幕后操作手机客户端，屏幕上看到的黑白时间定时界面就是我风雨不动摇的黑白风格了。'
+        caption: '编程进行了24个小时之后，到了我们《唤醒灯》成果展示的时间，鹏飞解说，我在幕后操作手机客户端，屏幕上看到的黑白时间定时界面就是我几百年不动摇的黑白风格了。'
       },
       {
-        imgUrl: './public/images/codejam02.JPG',
-        imgAlt: '2',
+        imgurl: './public/images/codejam02.JPG',
+        imgalt: '2',
         href: 'http://segmentfault.com/e/codejam-guangzhou-2013-p3',
-        caption: '灯已经慢慢亮到可以唤醒睡眠的人，大家都在等遥控关灯，结果android和单片机的连接超时已经断开，求原谅。'
+        caption: '灯已经慢慢亮到可以唤醒睡眠的人，大家都在等遥控关灯，结果android和单片机的连接超时，求原谅。'
       },
       {
-        imgUrl: './public/images/codejam03.JPG',
-        imgAlt: '3',
+        imgurl: './public/images/codejam03.JPG',
+        imgalt: '3',
         href: 'http://segmentfault.com/e/codejam-guangzhou-2013-p3',
-        caption: '演示出状况还是得了第四名，后来主办方计算了作品数量解释说第四名开始是优秀奖，好吧，就是第一名优秀奖。'
+        caption: '演示出状况得了第四名，后来主办方计算了作品数量解释说第四名开始是优秀奖，好吧，第一名优秀奖。'
       }
     ],
     intern: [
       {
-        isFirst: true,
-        imgUrl: './public/images/intern01.jpg',
-        imgAlt: '1',
-        caption: '“三个月实习之后就可以拿到学分了”, 怀抱着这样的心情迎来了UC的第一天。'
+        isfirst: true,
+        imgurl: './public/images/intern01.jpg',
+        imgalt: '1',
+        caption: '“实习之后就可以拿到学分了”, 抱着这样的心情在UC的第一天。'
       },
       {
-        imgUrl: './public/images/intern02.jpg',
-        imgAlt: '2',
-        caption: '一年零三个月陪伴我所有编程之路的操作系统，求助了linux大牛也没能成功编译tplink55××驱动，解决办法是换网卡。'
+        imgurl: './public/images/intern02.jpg',
+        imgalt: '2',
+        caption: '一年零三个月陪伴我所有编程之路的操作系统，求助了linux大神也没能成功编译tplink55××驱动，解决办法是换网卡。'
       },
       {
-        imgUrl: './public/images/intern03.jpg',
-        imgAlt: '3',
+        imgurl: './public/images/intern03.jpg',
+        imgalt: '3',
         caption: '他们很忙我很少和他们交流，这块地方总是停电和服务器down机，以至于最初的开发过程满满的都是技术和硬件的挑战。'
       },
       {
-        imgUrl: './public/images/intern04.jpg',
-        imgAlt: '4',
-        caption: '参加了四个线上活动的应用研发，参与Java后台又参与Javascript前端，加上没用心，我不是反应迟钝，相信我！'
+        imgurl: './public/images/intern04.jpg',
+        imgalt: '4',
+        caption: '参与四个线上活动的Java后台和Javascript前端，加上没用心，我不是反应迟钝，相信我！'
       },
       {
-        imgUrl: './public/images/intern05.jpg',
-        imgAlt: '5',
+        imgurl: './public/images/intern05.jpg',
+        imgalt: '5',
         caption: '没有强烈留在公司的愿望成真了。<br>“推广产品和推倒妹子”一点也不好笑了。'
       },
       {
-        imgUrl: './public/images/intern06.jpg',
-        imgAlt: '6',
+        imgurl: './public/images/intern06.jpg',
+        imgalt: '6',
         caption: '为当初的轻浮付出的代价——回不去的位置。'
       }
     ],
     hope: [
       {
-        isFirst: true,
-        imgUrl: './public/images/hope01.JPG',
-        imgAlt: '1',
+        isfirst: true,
+        imgurl: './public/images/hope01.JPG',
+        imgalt: '1',
         href: 'https://plus.google.com/photos/114920001331857622210/albums/5841418929094126033',
         caption: '不会孤立我。'
       },
       {
-        imgUrl: './public/images/hope02.JPG',
-        imgAlt: '1',
-        caption: '背影迷人，头发修理过。'
-      },
-      {
-        imgUrl: './public/images/hope03.jpg',
-        imgAlt: '1',
-        caption: '台上最闪。'
-      },
-      {
-        imgUrl: './public/images/hope04.JPG',
-        imgAlt: '1',
-        caption: '怎么抓拍都好看。'
-      },
-      {
-        imgUrl: './public/images/hope05.JPG',
-        imgAlt: '1',
+        imgurl: './public/images/hope05.JPG',
+        imgalt: '1',
         caption: '不被边缘化。'
       },
       {
-        imgUrl: './public/images/hope06.jpg',
-        imgAlt: '1',
-        caption: '数不清的朋友，4个，5个。。。。'
+        imgurl: './public/images/hope02.JPG',
+        imgalt: '1',
+        caption: '背影好看些。'
       },
       {
-        imgUrl: './public/images/hope07.jpg',
-        imgAlt: '1',
-        caption: '美丽回眸，长发飘飘。。'
+        imgurl: './public/images/hope03.jpg',
+        imgalt: '1',
+        caption: '注意形象。'
       },
       {
-        imgUrl: './public/images/hope08.jpg',
-        imgAlt: '1',
-        caption: '女神的智商。'
+        imgurl: './public/images/hope.jpg',
+        imgalt: '1',
+        caption: '吃的都长在该肥的地方。'
       },
       {
-        imgUrl: './public/images/hope09.jpg',
-        imgAlt: '1',
-        caption: '活在爱丽丝梦游仙境。'
+        imgurl: './public/images/hope04.JPG',
+        imgalt: '1',
+        caption: '不会营养不良。'
       },
       {
-        imgUrl: './public/images/hope10.jpg',
-        imgAlt: '1',
-        caption: '技术牛人，2个，3个屏幕'
+        imgurl: './public/images/hope07.jpg',
+        imgalt: '1',
+        caption: '有美丽回眸，长发飘飘。。'
       },
       {
-        imgUrl: './public/images/hope11.jpg',
-        imgAlt: '1',
-        caption: '吉他厉害。'
+        imgurl: './public/images/hope08.jpg',
+        imgalt: '1',
+        caption: '有女神的智商。'
       },
-      // {
-        // imgUrl: './public/images/hope.JPG',
-        // imgAlt: '1',
-        // caption: '父亲还在我身边'
-      // },
       {
-        imgUrl: './public/images/hope12.jpg',
-        imgAlt: '1',
-        caption: 'A man and a lady.'
+        imgurl: './public/images/hope06.jpg',
+        imgalt: '1',
+        caption: '4个，5个。。数不清的朋友。'
+      },
+      {
+        imgurl: './public/images/hope13.png',
+        imgalt: '1',
+        caption: '有永远的闺蜜。'
+      },
+      {
+        imgurl: './public/images/hope14.png',
+        imgalt: '1',
+        caption: '有人替我留念。'
+      },
+      {
+        imgurl: './public/images/hope10.jpg',
+        imgalt: '1',
+        caption: '技术牛人，控制2个，3个屏幕'
+      },
+      {
+        imgurl: './public/images/hope11.jpg',
+        imgalt: '1',
+        caption: '吉他很厉害。'
+      },
+      {
+        imgurl: './public/images/hope12.jpg',
+        imgalt: '1',
+        caption: 'To be a man and a lady.'
       }
     ]
   },
   ablum: [
     {
-      tagId: 0,
-      tagName: 'GZ第四届Hacker'
+   //和数据库select出来的id即tagid一致，所以不从0开始
+      tagid: 1,
+      tagname: 'GZ第四届Hacker'
     },
     {
-      tagId: 1,
-      tagName: '2013移动开发者',
+      tagid: 2,
+      tagname: '2013移动开发者',
     },
     {
-      tagId: 2,
-      tagName: 'UC暑假',
+      tagid: 3,
+      tagname: 'UC暑假',
       select: true
     },
     {
-      tagId: 3,
-      tagName: '希望与现实'
+      tagid: 4,
+      tagname: '希望与现实'
     }
   ]
   
